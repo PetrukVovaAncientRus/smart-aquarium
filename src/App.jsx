a7e87130d9677edca37bd0c81aebd63393c22652
Volodymyr Petruk <h1y2g3o4@gmail.com>
2026-06-10 16:04:12 +0200

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import LegacyLayout from "./LegacyLayout.jsx";
-import { useAquariumLogic } from "./useAquariumLogic.js";
-
-export default function App() {
-  useAquariumLogic();
-
-  return <LegacyLayout/>;
-}
