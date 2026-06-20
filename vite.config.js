c781adf690033756f9778a025cc8c373112443a4
Volodymyr Petruk <h1y2g3o4@gmail.com>
2026-06-20 14:44:58 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // важно для Docker
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://aquarium-backend:5000',
+        changeOrigin: true,
+      },
+      '/health': {
+        target: 'http://aquarium-backend:5000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
